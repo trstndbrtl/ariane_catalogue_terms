7d6c64c74c86210980b5fd65ff4e2fbdee4f7381
trstndbrtl <38991174+trstndbrtl@users.noreply.github.com>
2018-05-04 22:56:49 +0200

    Prepare the service

---
+++ src/ArianeService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ariane_catalogue_terms;
+
+/**
+ * Class ArianeService.
+ */
+class ArianeService implements ArianeServiceInterface {
+
+  /**
+   * Constructs a new ArianeService object.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ src/ArianeServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\ariane_catalogue_terms;
+
+/**
+ * Interface ArianeServiceInterface.
+ */
+interface ArianeServiceInterface {
+
+
+}
